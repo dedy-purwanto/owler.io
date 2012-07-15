aa5e1f3e046dbfb97f4feb3f2600e4c8b277622e
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 11:47:15 +0800

    Remove unused dependencies

--- owlerio/apps/hosts/models.py
+++ owlerio/apps/hosts/models.py
@@ -1,11 +1,7 @@
 from django.db import models
-from django.db.models import Q
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-from redis import Redis
-from preferences.models import Preference, cache_repo_preference
+from preferences.models import Preference
 
 
 class Host(models.Model):
