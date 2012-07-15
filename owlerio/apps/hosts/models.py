2c78f427be5c38581a322cdf6163bf9d13658261
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 11:44:27 +0800

    Add Host model

--- owlerio/apps/hosts/models.py
+++ owlerio/apps/hosts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from redis import Redis
+
+from preferences.models import Preference, cache_repo_preference
+
+
+class Host(models.Model):
+
+    slug = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.slug
