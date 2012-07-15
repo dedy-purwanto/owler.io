77ea8035c0b6ec9ac3426ec79055c785f42c2ce9
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 11:44:51 +0800

    Add UserHost model

--- owlerio/apps/hosts/models.py
+++ owlerio/apps/hosts/models.py
@@ -15,3 +15,24 @@ class Host(models.Model):
 
     def __unicode__(self):
         return self.slug
+
+class UserHost(models.Model):
+
+    host = models.ForeignKey(Host, related_name='users')
+    user = models.ForeignKey(User, related_name='user_host')
+    repo_username = models.CharField(max_length=255)
+    repo_userid = models.IntegerField(blank=True, null=True) 
+    access_token = models.TextField()
+    access_token_secret = models.TextField(blank=True, null=True) # For Oauth 1.0
+    preference = models.OneToOneField(Preference, related_name='user_host')
+
+    def save(self, *args, **kwargs):
+        try:
+            preference = self.preference
+        except Preference.DoesNotExist:
+            self.preference = Preference.objects.create()
+
+        super(UserHost, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return '%s - %s' % (self.user.username, self.host.slug)
