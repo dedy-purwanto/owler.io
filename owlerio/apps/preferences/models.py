e2cd1e0ee903be635e7589b011fa120928331d25
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 10:23:13 +0800

    Added preference basic model

--- owlerio/apps/preferences/models.py
+++ owlerio/apps/preferences/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from model_utils import Choices
 
-# Create your models here.
+class Preference(models.Model):
+
+    CHOICES = Choices(
+            (0, 'inherited', 'Inherited'),
+            (1, 'enabled', 'Enabled'),
+            (2, 'disabled', 'Disabled'),
+    )
+    track_request = models.IntegerField(choices=CHOICES, default=CHOICES.inherited)
+
+    # By enabling this, user will also get some error report (if exist) through web interface
+    track_response = models.IntegerField(choices=CHOICES, default=CHOICES.inherited)
+
+    email_errors = models.IntegerField(choices=CHOICES, default=CHOICES.inherited)
+    signature = models.IntegerField(choices=CHOICES, default=CHOICES.inherited)
+    signature_text = models.TextField(blank=True, null=True)
