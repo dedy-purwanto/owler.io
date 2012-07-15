ec7a7d256abfa302d2888d5bb0bfa853ab93f429
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 10:36:09 +0800

    Fix test case for admin and normal user
    Note that the second test case doesn't need to create admin since it
    was already generated in the first test case

--- owlerio/apps/accounts/tests.py
+++ owlerio/apps/accounts/tests.py
@@ -1,14 +1,26 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
+from accounts.models import Profile
+
 
 class ProfileTest(TestCase):
 
     def test_profile_exist_after_create_user(self):
+        # First user (admin, does not have profile)
+        user = User.objects.create(username="admin", password="123")
+        try:
+            self.assertEqual(user.profile, None)
+        except Profile.DoesNotExist:
+            pass
+
         user = User.objects.create(username="username", password="123")
-        self.assertNotEqual(user.profile, None)
+        try:
+            self.assertNotEqual(user.profile, None)
+        except Profile.DoesNotExist:
+            pass
+
 
     def test_profile_preference_created(self):
         user = User.objects.create(username="username", password="123")
-
         self.assertNotEqual(user.profile.preference, None)
