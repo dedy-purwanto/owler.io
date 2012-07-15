a2f48b01501849e7e8d6003aff788bb0f1f337c0
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 10:12:03 +0800

    Added profile model

--- owlerio/apps/accounts/models.py
+++ owlerio/apps/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from preferences.models import Preference
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, related_name='profile')
+    preference = models.OneToOneField(Preference, related_name='profile')
+
+    def save(self, *args, **kwargs):
+        try:
+            preference = self.preference
+        except Preference.DoesNotExist:
+            self.preference = Preference.objects.create(
+                    track_request=Preference.CHOICES.disabled,
+                    track_response=Preference.CHOICES.disabled,
+                    email_errors=Preference.CHOICES.enabled,
+                    signature=Preference.CHOICES.disabled,
+                    signature_text="Sent from owler.io",
+                    )
+        super(Profile, self).save(*args, **kwargs)
