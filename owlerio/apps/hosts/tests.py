bf1102ac642b992b36b8eb93e71caa4360810a9b
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 23:47:08 +0800

    Line wraps fix

--- owlerio/apps/hosts/tests.py
+++ owlerio/apps/hosts/tests.py
@@ -6,10 +6,11 @@ from hosts.models import Host, UserHost, UserRepo
 class RepoTest(TestCase):
 
     """
-    Create 2 repos with same name (and also alias) for a user, revalidate,
-    and the they are all should become invalid.
+    Create 2 repos with same name (and also alias) for a user, 
+    revalidate, and the they are all should become invalid.
 
-    if another user has 1 repo with same name as the first user, it'll pass
+    if another user has 1 repo with same name as the first user, 
+    it'll pass
     """
     def test_duplicate_repo_name_and_alias(self):
         github = Host.objects.create(slug='github')
@@ -18,19 +19,28 @@ class RepoTest(TestCase):
         user_a = User.objects.create(username='a', password='123')
         user_b = User.objects.create(username='b', password='123')
 
-        host_a_github = UserHost.objects.create(host=github, user=user_a, 
-                repo_username=user_a.username, repo_userid=user_a.pk, access_token='123')
-        host_a_bitbucket = UserHost.objects.create(host=bitbucket, user=user_a, 
-                repo_username=user_a.username, repo_userid=user_a.pk, access_token='123')
+        host_a_github = UserHost.objects.create(host=github, 
+                user=user_a, repo_username=user_a.username, 
+                repo_userid=user_a.pk, access_token='123')
+        host_a_bitbucket = UserHost.objects.create(host=bitbucket,
+                user=user_a, 
+                repo_username=user_a.username, repo_userid=user_a.pk,
+                access_token='123')
         host_b = UserHost.objects.create(host=github, user=user_b, 
-                repo_username=user_b.username, repo_userid=user_b.pk, access_token='123')
+                repo_username=user_b.username, repo_userid=user_b.pk,
+                access_token='123')
 
-        user_a_repo_a1 = UserRepo.objects.create(user_host=host_a_github, repo_id=1, title='repo_a')
-        user_a_repo_a2 = UserRepo.objects.create(user_host=host_a_bitbucket, repo_id=2, title='repo_a')
-        user_a_repo_b = UserRepo.objects.create(user_host=host_a_github, repo_id=3, title='repo_b')
+        user_a_repo_a1 = UserRepo.objects.create(user_host=host_a_github,
+                repo_id=1, title='repo_a')
+        user_a_repo_a2 = UserRepo.objects.create(user_host=host_a_bitbucket,
+                repo_id=2, title='repo_a')
+        user_a_repo_b = UserRepo.objects.create(user_host=host_a_github,
+                repo_id=3, title='repo_b')
 
-        user_b_repo_a = UserRepo.objects.create(user_host=host_b, repo_id=1, title='repo_a')
-        user_b_repo_b = UserRepo.objects.create(user_host=host_b, repo_id=2, title='repo_b')
+        user_b_repo_a = UserRepo.objects.create(user_host=host_b,
+                repo_id=1, title='repo_a')
+        user_b_repo_b = UserRepo.objects.create(user_host=host_b, repo_id=2,
+                title='repo_b')
 
         self.assertEqual(user_a_repo_a1.is_valid, True)
         self.assertEqual(user_a_repo_a2.is_valid, False)
@@ -46,18 +56,26 @@ class RepoTest(TestCase):
         user_b = User.objects.create(username='b', password='123')
 
         host_a_github = UserHost.objects.create(host=github, user=user_a, 
-                repo_username=user_a.username, repo_userid=user_a.pk, access_token='123')
+                repo_username=user_a.username, repo_userid=user_a.pk,
+                access_token='123')
         host_a_bitbucket = UserHost.objects.create(host=bitbucket, user=user_a, 
-                repo_username=user_a.username, repo_userid=user_a.pk, access_token='123')
+                repo_username=user_a.username, repo_userid=user_a.pk,
+                access_token='123')
         host_b = UserHost.objects.create(host=github, user=user_b, 
-                repo_username=user_b.username, repo_userid=user_b.pk, access_token='123')
+                repo_username=user_b.username, repo_userid=user_b.pk,
+                access_token='123')
 
-        user_a_repo_a1 = UserRepo.objects.create(user_host=host_a_github, repo_id=1, title='repo_1')
-        user_a_repo_a2 = UserRepo.objects.create(user_host=host_a_bitbucket, repo_id=2, title='repo_2')
-        user_a_repo_b = UserRepo.objects.create(user_host=host_a_github, repo_id=3, title='repo_3')
+        user_a_repo_a1 = UserRepo.objects.create(user_host=host_a_github,
+                repo_id=1, title='repo_1')
+        user_a_repo_a2 = UserRepo.objects.create(user_host=host_a_bitbucket,
+                repo_id=2, title='repo_2')
+        user_a_repo_b = UserRepo.objects.create(user_host=host_a_github,
+                repo_id=3, title='repo_3')
 
-        user_b_repo_a = UserRepo.objects.create(user_host=host_b, repo_id=1, title='repo_1')
-        user_b_repo_b = UserRepo.objects.create(user_host=host_b, repo_id=2, title='repo_2')
+        user_b_repo_a = UserRepo.objects.create(user_host=host_b, repo_id=1,
+                title='repo_1')
+        user_b_repo_b = UserRepo.objects.create(user_host=host_b, repo_id=2,
+                title='repo_2')
 
         self.assertEqual(user_a_repo_a1.is_valid, True)
         self.assertEqual(user_a_repo_a2.is_valid, True)
