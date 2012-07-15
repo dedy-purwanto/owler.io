755eb1d75ccd39491481ee62fed26cdda7c79fe5
Dedi Purwanto <kecebongsoft@gmail.com>
2012-07-15 10:57:40 +0800

    Added frontpage view

--- owlerio/apps/home/views.py
+++ owlerio/apps/home/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
+class HomeView(TemplateView):
+
+    template_name = "home/home.html"
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.user.is_authenticated():
+            return redirect(reverse("accounts:dashboard"))
+
+        return super(HomeView, self).render_to_response(context, **response_kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+
+        return context
